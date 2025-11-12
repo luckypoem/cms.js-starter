618fcfb7a4c9e551a769bd4e2932140f74575e4e
luckypoem <luckypoem@gmail.com>
2025-11-12 00:06:11 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,12 +11,12 @@ var config = {
   // If Github mode is set, your Github username, repo name, 
   // and branch to get files from.
   github: {
-    username: 'yourusername',
-    repo: 'yourrepo',
+    username: 'luckypoem',
+    repo: 'cms.js-starter',
     branch: 'gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
-    // prefix: 'subdirectory',
+    // prefix: 'cms.js-starter',
   },
 
   // The name of the layouts directory.
