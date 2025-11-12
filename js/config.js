184351b47c009fe768e7b661b3448d096f43916e
luckypoem <luckypoem@gmail.com>
2025-11-12 00:09:28 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var config = {
   github: {
     username: 'luckypoem',
     repo: 'cms.js-starter',
-    branch: 'gh-pages',
+    branch: 'develop',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
     // prefix: 'cms.js-starter',
